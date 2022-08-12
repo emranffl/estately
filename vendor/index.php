b0c5e96f20a31f14b5f778e6a02a0715ed901514
Saleh Ahmed <salehahmed@Samias-MacBook-Pro.local>
2022-08-12 16:25:18 +0600

    Vendor Frontend Deployed🚀

--- vendor/index.php
+++ vendor/index.php
@@ -9,52 +9,92 @@ try {
     consoleError($e->getMessage());
 }
 
-consoleLog($var);
+// consoleLog($var);
 
 // close connection
 R::close();
 ?>
 
 <!DOCTYPE html>
-
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vendor Login | Estately</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/vendor-signup-signin.css">
 </head>
-<?php require('../layouts/vendor/headernav.php') ?>
-
 <body>
 
-    <section class="container p-5">
-        <div class="row row-cols-1 row-cols-lg-2 mx-0 p-5">
-            <div class="col p-5">
-                <form class="">
-                    <div class="mb-2">
-                        <label for="loginEmail" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="loginEmail" placeholder="example@domain.com" required>
-                        <small class="invalid-feedback" id="loginEmailFeedback"></small>
+    <section class="container-fluid vh-100 vw-100">
+        <div class="row h-100 p-2 d-flex">
+            <div class="col col-12 col-lg-3 d-lg-flex flex-column justify-content-between gap-5 p-4" id="left">
+                <div class="top">
+                    <a href="../index.php">
+                        <h1 class="fs-4 fw-bold">Estately</h1>
+                    </a>
+                </div>
+                <div class="mid">
+                    <h1 class="fs-1 fw-bold lh-1">Start your jouney with us.</h1>
+                    <p class="mb-0">Discover the world's best real estate management service.</p>
+                </div>
+                <div class="bot d-none d-lg-block">
+                    <div id="carouselExampleSlidesOnly" class="carousel slide carousal-item-bg border-radius-12 p-3" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <p>Amazing service, simply. Would 10/10 recommend. Estately has allowed me to carry my business to the next level.</p>
+                                <div class="reviewer d-flex align-items-center gap-3">
+                                    <div class="reviewer-image" id="item1"></div>
+                                    <div class="reviewer-info d-flex flex-column">
+                                        <h1 class="mb-0 fs-5">Stacey Thewys</h1>
+                                        <p class="mb-0 fs-6 text-muted">Senior Sales Associate</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item ">
+                                <p>Fantastic customer support and super satisfying UI. Love using this piece of masterpiece for management.</p>
+                                <div class="reviewer d-flex align-items-center gap-3">
+                                    <div class="reviewer-image" id="item2"></div>
+                                    <div class="reviewer-info d-flex flex-column">
+                                        <h1 class="mb-0 fs-5 fw-bold">Cathy Goodwyn</h1>
+                                        <p class="mb-0 fs-6 text-muted">Tax Accountant</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item ">
+                                <p>Excellently built system, and has allowed me to seamlessly keep track of my properties, with very good UX.</p>
+                                <div class="reviewer d-flex align-items-center gap-3">
+                                    <div class="reviewer-image" id="item3"></div>
+                                    <div class="reviewer-info d-flex flex-column">
+                                        <h1 class="mb-0 fs-5 fw-bold">Randie Cello</h1>
+                                        <p class="mb-0 fs-6 text-muted">Executive Secretary</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
-                    <div class="mb-4">
-                        <label for="loginPassword" class="form-label">Password</label>
-                        <input type="text" class="form-control" id="loginPassword" placeholder="4-25 characters" required>
-                        <small class="invalid-feedback" id="loginPasswordFeedback"></small>
+                </div>
+            </div>
+            <div class="col col-12 col-lg-9 p-4 p-lg-5" id="right">
+                <h1 class="mb-3 fs-3">Login to you vendor account with Estately.</h1>
+                <p class="mb-3">Don't have an accounnt? <a href="./signup.php">Signup</a> instead.</p>
+                <!-- Signup form -->
+                <form class="" id="signup-form">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label mb-0">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
-
-                    <a href="reset.php" class="btn ps-0 pt-0 link-danger">Forgot password?</a>
-
-                    <div class="text-center text-md-start mt-2 pt-2">
-                        <button type="submit" class="btn btn-sm btn-primary w-75 w-md-100">Login</button>
-                        <small class="d-block fw-bold mt-2">Don't have an account? <a href="signup.php" class="btn ps-0 pt-0 link-primary">Sign Up</a></small>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label mb-0">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
                     </div>
-
+                    <button type="submit" class="btn custom-btn px-5 py-3 shadow-none fw-bold">Login</button>
                 </form>
             </div>
-            <div class="col">
-                <img src="resources/images/login-decoration-image.webp" class="img-fluid" alt="decoration image">
-            </div>
         </div>
-
     </section>
 
-    <?php require('../layouts/vendor/footer.php') ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+</html>
--- vendor/signup.php
+++ vendor/signup.php
@@ -9,7 +9,7 @@ try {
     consoleError($e->getMessage());
 }
 
-consoleLog($var);
+// consoleLog($var);
 
 // close connection
 R::close();
@@ -20,9 +20,9 @@ R::close();
 <head>
     <title>Vendor Signup | Estately</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="./styles/vendor-signup.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="./css/vendor-signup-signin.css">
 </head>
 
 <body>
@@ -101,7 +101,7 @@ R::close();
                         <label for="exampleInputEmail1" class="form-label mb-0">Document ID</label>
                         <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
-                    <button type="submit" class="btn px-5 py-3 shadow-none fw-bold">Create Account</button>
+                    <button type="submit" class="btn custom-btn px-5 py-3 shadow-none fw-bold">Create Account</button>
                 </form>
             </div>
         </div>
